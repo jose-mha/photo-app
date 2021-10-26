7e9adb67522feb2d25551742f060ce7207ffb4ba
jose-mha <josemhha@gmail.com>
2021-10-26 17:45:22 -0500

    :art: Principios de responsabilidad unica
    Principios de responsabilidad única

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
-import { Album, PhotoOrientation, Picture, User } from "./photo-app";
+import { Album } from "./album";
+import { PhotoOrientation } from "./photo-orientation";
+import { Picture } from "./picture";
+import { User } from "./user";
 
 const user = new User(1, 'josemha', 'Jose', true);
 const album = new Album(10, 'Platzi Pictures');
@@ -8,4 +11,8 @@ const picture = new Picture(1, 'TypeScript Course', '2020-03', PhotoOrientation.
 user.addAlbum(album);
 album.addPicture(picture);
 
+console.log('user', user);
+
+// Borramos album
+user.removeAlbum(album);
 console.log('user', user);
---
+++ src/photo-orientation.ts
@@ -0,0 +1,6 @@
+export enum PhotoOrientation{
+	Lanscape,
+	Portrait,
+	Square,
+	Panorama
+}
